87e553bbe72b7db8166727c5b8e28b1a98255bc1
mevans81 <mevans@hampton.k12.va.us>
2026-02-02 17:15:20 -0500

    PathPlanner Settings

--- 2026CompBot/src/main/java/frc/robot/subsystems/AprilTags.java
+++ 2026CompBot/src/main/java/frc/robot/subsystems/AprilTags.java
@@ -21,6 +21,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.numbers.N8;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
