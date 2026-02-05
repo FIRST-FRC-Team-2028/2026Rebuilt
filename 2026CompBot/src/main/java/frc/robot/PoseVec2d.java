3839217edea8ea8e8b28a2cea705370310b906b8
clydetheSlide <mizugana@gmail.com>
2026-02-05 09:33:33 -0500

    added example to PoseVec2d

--- 2026CompBot/src/main/java/frc/robot/PoseVec2d.java
+++ 2026CompBot/src/main/java/frc/robot/PoseVec2d.java
@@ -1,10 +1,13 @@
 package frc.robot;
 
+import edu.wpi.first.apriltag.AprilTag;
+import edu.wpi.first.math.Matrix;
 //import edu.wpi.first.math.Matrix;
 //import edu.wpi.first.math.Nat;
 //import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.numbers.N3;
 
 /**Provide 2D vector arithmetic methods to treat Pose2D objects like vectors 
      * <p> ie, addition and subtraction are component operations - yes all three, including the angles
@@ -31,19 +34,28 @@ public class PoseVec2d
         x[2]=me.getRotation().getRadians();
     }
 
+    /** Construct new PoseVec2d from componentscomponents
+     * <p> Please provide angle in radians
+     * @param x
+     * @param y
+     * @param t
+     */
     public PoseVec2d(double x, double y, double t){
         this.x[0]=x;
         this.x[1]=y;
         this.x[2]=t;
     }
 
+    /**Construct Pose2d from this "vector" */
     public Pose2d toPose2d() {
         return new Pose2d(x[0],x[1],new Rotation2d(x[2]));
     }
 
-    /** return component of this "vector" */
+    /** return X component of this "vector" */
     public double X(){return x[0];}
+    /** return Y component of this "vector" */
     public double Y(){return x[1];}
+    /** return angle component of this "vector" */
     public double T(){return x[2];}
 
     /**Add components of addend to this vector and return the resulting new vector */
@@ -62,10 +74,14 @@ public class PoseVec2d
                             );
     }
 
+    /**Compute dot product with
+     * @param dotee
+     */
     public double dot(PoseVec2d dotee) {
         return x[0]*dotee.X() + x[0]*dotee.Y();
     }
 
+    /**Compute second norm  */
     public  double norm() {
         //return x[0]*x[0] + x[1]*x[1];
         return this.dot(this);
@@ -83,11 +99,30 @@ public class PoseVec2d
                             );
     }
 
+    /**Generate vector of length 1 in the same direction*/
     public PoseVec2d unit() {
         return scalarProd(1./norm());
     }
 
+    /** Compute inner product */
     public double cross(PoseVec2d posee) {
         return x[0]*posee.Y() - x[1]*posee.X();
     }
+
+    /** transform (not done yet)
+    public PoseVec2d transform(Matrix<N3,N3> trans){
+        PoseVec2d res = new PoseVec2d(this.dot(trans.extractColumnVector(0)),
+                                      this.dot(trans.extractColumnVector(1)),
+                                      this.dot(trans.extractColumnVector(2)));
+    } */
+
+    /* example of use 
+     *  dest = start + (total dist - range) * ||(hub - whereIam)||
+     * 
+    void goTorange() {
+        PoseVec2d diff = new PoseVec2d(AprilTag.getPose2d()).minus(whereIam);
+        double dist = diff.norm() - desiredRange;
+        PoseVec2d whereToGo = whereIam.plus(diff.unit().scalarProd(dist));  // this doesn't change the heading
+    }
+    */
 }
